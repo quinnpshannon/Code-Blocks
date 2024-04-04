30a0f9790267a1715efc7e344c31aec70db1f1e3
Quinn Shannon <quinn.p.shannon@gmail.com>
2024-04-04 15:32:34 -0400

    Plants is Index, New stuff is Index2

--- js/script.js
+++ js/script.js
@@ -18,34 +18,34 @@ The global scope is represented by the Window object
 /*
 ---BLOCK SCOPE---
 */
-console.log("Example 1");
-console.log("Example 1 causes bugs in the rest. It is commented out");
+// console.log("Example 1");
+// console.log("Example 1 causes bugs in the rest. It is commented out");
+// // {
+// //     var x=1;
+// //     {
+// //         var x=2;
+// //         console.log(x); // value is 2
+// //     }
+// //     console.log(x); // value is STILL 2! The scope is GLOBAL not BLOCK
+// //     // This is B A D
+// // }
+// console.log("Example 2");
 // {
-//     var x=1;
+//     let x=1;
 //     {
-//         var x=2;
+//         let x=2;
 //         console.log(x); // value is 2
 //     }
-//     console.log(x); // value is STILL 2! The scope is GLOBAL not BLOCK
-//     // This is B A D
+//     console.log(x); // value is 1! Things are Scoped Properly!
+//     // This is G O O D
 // }
-console.log("Example 2");
-{
-    let x=1;
-    {
-        let x=2;
-        console.log(x); // value is 2
-    }
-    console.log(x); // value is 1! Things are Scoped Properly!
-    // This is G O O D
-}
-console.log("Example 3");
-let x=10;
-let y=5;
-{
-    let z=x+y;
-    console.log(z);
-}
+// console.log("Example 3");
+// let x=10;
+// let y=5;
+// {
+//     let z=x+y;
+//     console.log(z);
+// }
 // console.log(z); // This throws an error, because it can't go into Children looking for scope
 
 // let age = prompt("How old are you?");
@@ -58,61 +58,155 @@ let y=5;
 //     console.log("Access Denied!");
 // }
 
-let num=75;
-if(num >100 && num > 0) {
-    console.log(`num: ${num} is Positive and greater than 100`);
-} else if (num<=100 && num > 0) {
-    console.log(`num: ${num} is Positive and less than or equal to 100`);
-}else if (num < 0) {
-    console.log(`num: ${num} is Negative`);
-}else {
-    console.log(`num: ${num} is Zero`);
-}
+// let num=75;
+// if(num >100 && num > 0) {
+//     console.log(`num: ${num} is Positive and greater than 100`);
+// } else if (num<=100 && num > 0) {
+//     console.log(`num: ${num} is Positive and less than or equal to 100`);
+// }else if (num < 0) {
+//     console.log(`num: ${num} is Negative`);
+// }else {
+//     console.log(`num: ${num} is Zero`);
+// }
 
-let grade=100;
-if(grade >=90) {
-    console.log("A");
-} else if (grade >= 80) {
-    console.log("B");
-}else if (grade >= 70) {
-    console.log("C");
-}else if (grade >= 55) {
-    console.log("D");
-}else {
-    console.log("F");
-}
+// let grade=100;
+// if(grade >=90) {
+//     console.log("A");
+// } else if (grade >= 80) {
+//     console.log("B");
+// }else if (grade >= 70) {
+//     console.log("C");
+// }else if (grade >= 55) {
+//     console.log("D");
+// }else {
+//     console.log("F");
+// }
 //THis is a cool comment to make a change to this file.
 
-let num1 = parseInt(prompt('Enter a number:'));
-let op = prompt('Enter a number:');
-let num2 = parseInt(prompt('Enter a number:'));
-let result;
+// let num1 = parseInt(prompt('Enter a number:'));
+// let op = prompt('Enter a number:');
+// let num2 = parseInt(prompt('Enter a number:'));
+// let result;
 
-switch (op) {
-    case '+':
-        result = num1 + num2;
-        break;
-    case '-':
-        result = num1 - num2;
-        break;
-    case '*':
-        result = num1 * num2;
-        break;
-    case '/':
-        if(num2 == 0){
-            result="You cannot divide by zero!";
-        } else {
-        result = num1 / num2;
-        }
-        break;
-    case '%':
-        if(num2 == 0){
-            result="You cannot divide by zero!";
-        } else {
-            result = num1 % num2;
-        }
-        break;
-    default:
-        result="Not a valid operand!"
+// switch (op) {
+//     case '+':
+//         result = num1 + num2;
+//         break;
+//     case '-':
+//         result = num1 - num2;
+//         break;
+//     case '*':
+//         result = num1 * num2;
+//         break;
+//     case '/':
+//         if(num2 == 0){
+//             result="You cannot divide by zero!";
+//         } else {
+//         result = num1 / num2;
+//         }
+//         break;
+//     case '%':
+//         if(num2 == 0){
+//             result="You cannot divide by zero!";
+//         } else {
+//             result = num1 % num2;
+//         }
+//         break;
+//     default:
+//         result="Not a valid operand!"
+// }
+// console.log(result);
+
+console.log("========== PART 1 ==========");
+let radius = 5;
+const PI = 3.1415;
+const area = PI * radius * radius;
+const plantSpace = .8;
+let plants = 20;
+let weeks = 5;
+let plantArea;
+let gardenStatus;
+
+for(let x=1;x<=weeks;x++){
+    plantArea = (plants*plantSpace)/area;
+    if(plantArea >= .8){
+        gardenStatus = 'Pruned';
+        x+=weeks;
+    } else if(plantArea >= .5){
+        gardenStatus = 'Monitored';
+    } else {
+        gardenStatus = 'Planted';
+    }
+    console.log(`Week ${x}`);
+    console.log(`There are currently ${plants} plants.`);
+    console.log(`Plants need to be ${gardenStatus}.`);
+    console.log(`They are occupying  ${plantArea*100}% of the space.`);
+    console.log(`====================`);
+    plants*=2;
 }
-console.log(result);
+console.log("========== PART 2 ==========");
+weeks = 10;
+plants = 100;
+areaCalc = PI * radius * radius;
+for(let x=1;x<=weeks;x++){
+    plantArea = (plants*plantSpace)/area;
+/*    if(plantArea >= .8){
+        gardenStatus = 'Pruned';
+        // x+=weeks;
+    } else if(plantArea >= .5){
+        gardenStatus = 'Monitored';
+    } else {
+        gardenStatus = 'Planted';
+    }
+    */
+    console.log(`Week ${x}`);
+    console.log(`There are currently ${plants} plants.`);
+    console.log(`They are occupying  ${plants*plantSpace} square meters.`);
+    console.log(`====================`);
+    plants*=2;
+}
+plants/=2;
+console.log(`There are currently ${plants} plants.`);
+console.log(`To have this many plants without pruning for ${weeks} weeks`);
+console.log(`you would need to have ${(plants*plantSpace)/.8} square meters.`);
+ //Oh huh, that's the same .8 I could probably trim both numbers, but this leaves it open to modifications
+console.log(`That's a circle with a radius of ${Math.sqrt((plants*plantSpace)/.8)} meters.`);
+
+console.log("========== PART 3 ==========");
+weeks = 10;
+plants = 100;
+areaCalc = PI * radius * radius;
+for(let x=1;x<=weeks;x++){
+    plantArea = (plants*plantSpace)/area;
+/*    if(plantArea >= .8){
+        gardenStatus = 'Pruned';
+        // x+=weeks;
+    } else if(plantArea >= .5){
+        gardenStatus = 'Monitored';
+    } else {
+        gardenStatus = 'Planted';
+    }
+    */
+    console.log(`Week ${x}`);
+    console.log(`There are currently ${plants} plants.`);
+    console.log(`They are occupying  ${plants*plantSpace} square meters.`);
+    console.log(`====================`);
+    plants*=2;
+}
+plants/=2;
+console.log(`There are currently ${plants} plants.`);
+console.log(`To have this many plants without pruning for ${weeks} weeks`);
+console.log(`you would need to have ${(plants*plantSpace)/.8} square meters.`);
+ //Oh huh, that's the same .8 I could probably trim both numbers, but this leaves it open to modifications
+console.log(`That's a circle with a radius of ${Math.sqrt((plants*plantSpace)/.8)} meters.`);
+//let plantArea = plants * plantSpace;
+//let result = plantArea / area;
+// console.log(result);
+// plants *=2;
+// plantArea = plants * plantSpace;
+// result = plantArea / area;
+// console.log(result);
+// plants *=2;
+// plantArea = plants * plantSpace;
+// result = plantArea / area;
+// console.log(result);
---
+++ js/script2.js
@@ -0,0 +1,74 @@
+//Ooooook, let's start over with the new assignment
+console.log(`===== Section 1 =====`)
+// Section 1
+// 1. Variables & Data types
+// A. Variables
+// Declare a new constant variable called myName and assign it your name?
+const myName = "Quinn Shannon";
+// Declare a variable called favoriteLanguage and assign it the value of Python?
+let favoriteLanguage = "Python";
+// Declare a new variable called newFavoriteLanguage and assign it JavaScript;
+let newFavoriteLanguage = "JavaScript";
+// Assign the value of newFavoriteLanguage to favoriteLanguage referencing each other.
+favoriteLanguage = newFavoriteLanguage;
+// Finally log or print a message using a Literal String, make use of the variables myName and favoriteLanguage.
+console.log(`My name is ${myName} and my favorite programming language is ${favoriteLanguage}!`);
+// B. Strings
+// Create a variable called firstVariable
+// Assign it the value of the string "Hello World"
+let firstVariable = "Hello World";
+// Change the value of this variable to some number
+firstVariable = 777;
+// Store the value of firstVariable in a new variable called secondVariable
+let secondVariable = firstVariable;
+// Change the value of secondVariable to any string.
+firstVariable = "String, Back again!";
+// What is the value of firstVariable? Log it!
+console.log(firstVariable);
+console.log(typeof firstVariable);
+console.log(secondVariable);
+console.log(typeof secondVariable);
+
+
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated. ex: Hello, my name is Jean Valjean
+yourName = myName;
+inigo = "Hello, my name is ";
+prepare = inigo+yourName;
+console.log(prepare);
+// C. Booleans
+// Using the provided variable definitions, replace the blanks so that all log statements print true in the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console:
+
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a <= b);
+  console.log(c >= d);
+  console.log('Name' == 'Name');
+  // FOR THE NEXT TWO, USE ONLY && OR ||
+  console.log(true || false);
+  console.log(false || false || false || false || false || true);
+  console.log(false == false)
+  console.log(e == 'Kevin');
+  console.log(a + b === c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+  console.log(a * a === d); // note: the answer is a simple arithmetic equation, not something "weird"
+  console.log(48 == '48');
+// D. The farm
+// Declare a variable animal. Set it to be either "cow" or something else
+const animal = 'cow ';
+// Write code that will print out "mooooo" if the it is equal to cow
+if (animal.trim().toLowerCase() === 'cow'){
+    console.log("mooooo")
+} else {console.log("Hey! You're not a cow.")}
+// Change your code so that if the variable animal is anything other than a cow, it will print "Hey! You're not a cow." Commit
+// E. Driver's Ed
+// Make a variable that holds a person's age; be semantic
+// Write code that will print out "Here are the keys!", if the age is 16 years or older, or, if the age is younger than 16, a message should print "Sorry, you're too young."
+let driversAge = 16;
+if (driversAge >= 16){
+    console.log("Here are the keys!");
+} else {
+    console.log("Sorry, you're too young.");
+}
