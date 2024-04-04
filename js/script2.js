1b9f43592b1849e9112303032440f535febea3c7
Quinn Shannon <quinn.p.shannon@gmail.com>
2024-04-04 15:42:24 -0400

    Finished Total Recall PS

--- js/script2.js
+++ js/script2.js
@@ -72,3 +72,102 @@ if (driversAge >= 16){
 } else {
     console.log("Sorry, you're too young.");
 }
+
+
+// A. Talk about it in your group:
+// What are the things in an array called?
+// Do Arrays guarantee those things will be in order?
+// What real-life thing could you model with an array?
+// B. Easy Does It
+// Create an array that contains three quotes and store it in a variable called quotes.
+// C. Accessing elements
+// Given the following array const randomThings = [1, 10, "Hello", true]
+
+// How do you access the 1st element in the array?
+
+// Change the value of "Hello"to "World"
+
+// Check the value of the array to make sure it updated the array. How? Why, yes! console.log();
+
+// D. Change values
+// Given the following array const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"];
+
+// What would you write to access the 3rd element of the array?
+
+// Change the value of "Github" to "Octocat"
+
+// Add a new element, "Cloud City" to the array
+
+// E. Mix It Up
+// Note: You don't really need .splice()for these. You could use it, but there are simpler array methods that are more appropriate.
+// Given the following array: const myArray = [5, 10, 500, 20]
+
+// Add the string "Aegon"to the end of the array. Add another string of your choice to the end of the array.
+// Remove the 5from the beginning of the array.
+// Add the string "Bob Marley"to the beginning of the array.
+// Remove the string of your choice from the end of the array.
+// Reverse this array using Array.prototype.reverse(). Did you mutate the array? What does mutate mean? Did the .reverse()method return anything?
+
+// F. Biggie Smalls
+// Create a variable that contains an integer.
+let aVariable = secondVariable;
+// Write an if ... else statement that:
+if (aVariable < 100){
+console.log("little number"); // if the number is entered is less than 100
+} else {
+console.log("big number");   // if the number is greater than or equal to 100.
+}
+// G. Monkey in the Middle
+// Write an if ... else if ... else statement:
+aVariable = 7; //Let's change it so we can display Monkey.
+if(aVariable < 5) {
+console.log("little number"); // if the number entered is less than 5.
+} else if (aVariable > 10){
+    // If the number entered is more than 10, log 
+    console.log("big number");
+} else {
+    console.log("monkey"); //Otherwise, log "monkey".
+}
+
+// Not just yet!
+// H. What's in Your Closet?
+// Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript.
+
+// const kristynsCloset = [
+//   "left shoe",
+//   "cowboy boots",
+//   "right sock",
+//   "Per Scholas hoodie",
+//   "green pants",
+//   "yellow knit hat",
+//   "marshmallow peeps"
+// ];
+// // Thom's closet is more complicated. Check out this nested data structure!!
+
+// const thomsCloset = [
+//   [
+//     // These are Thom's shirts
+//     "grey button-up",
+//     "dark grey button-up",
+//     "light blue button-up",
+//     "blue button-up",
+//   ],[
+//     // These are Thom's pants
+//     "grey jeans",
+//     "jeans",
+//     "PJs"
+//   ],[
+//     // Thom's accessories
+//     "wool mittens",
+//     "wool scarf",
+//     "raybans"
+//   ]
+// ];
+// What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+// Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
+// Kristyn spilled coffee on her hat... modify this item to read "stained knit hat"instead of yellow.
+// Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
+// In the same way, access one item from Thom's pants array.
+// Access one item from Thom's accessories array.
+// Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+// Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
